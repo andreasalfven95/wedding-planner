66e2fc330fc83558ca48207bf1367e453b73841d
Andreas Alfvén <andreas.larsson95@gmail.com>
2021-02-26 19:08:56 +0100

    Remaking the navbar with Tailwind instead of styled components. Currently working on dropdown for future admin-user.

--- components/Button.js
+++ components/Button.js
@@ -2,27 +2,23 @@ import styled from 'styled-components'
 import Link from 'next/link'
 
 export const Button = styled.div`
-  background: ${({ primary }) => (primary ? '#F26A2E' : '#077BF1')};
+  background: ${({ primary }) => (primary ? '#aaa' : 'transparent')};
+  color: ${({ primary }) => (primary ? '#fff' : '#000')};
   white-space: nowrap;
-  padding: ${({ big }) => (big ? '16px 40px' : '10px 32px')};
-  color: #fff;
-  font-size: ${({ big }) => (big ? '20px' : '16px')};
+  padding: ${({ big }) => (big ? '0px 1.5rem' : '0px 1rem')};
   outline: none;
-  border: none;
-  min-width: 100px;
+  border: ${({ primary }) => (primary ? 'none' : '1px solid #ccc')};
   cursor: pointer;
   text-decoration: none;
   transition: 0.3s !important;
   border-radius: ${({ round }) => (round ? '50px' : 'none')};
-  /* margin: 0 1rem; */
   height: 40px;
   display: flex;
   align-items: center;
   width: min-content;
 
   &:hover {
-    /* background: ${({ primary }) => (primary ? '#077BF1' : '#F26A2E')};
-    transform: translateY(-2px); */
-    transform: scale(1.075);
+    background: ${({ primary }) => (primary ? '#777' : '#ddd')};
+    /* transform: scale(1.075); */
   }
 `
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import Link from 'next/link'
 import styled from 'styled-components'
 import { FaBars } from 'react-icons/fa'
+import { GrLogout } from 'react-icons/gr'
+import { VscSignIn } from 'react-icons/vsc'
 import { menuData } from '../data/MenuData'
 import { Button } from './Button'
 import { DataContext } from '../store/GlobalState'
@@ -22,95 +24,159 @@ const Navbar = ({ toggle }) => {
 
   const loggedRouter = () => {
     return (
-      <NavBtn>
+      <div className='items-center flex'>
         <Link href='/profile' passHref>
-          <Button
-            primary='true'
-            round='true'
-            style={{
-              marginRight: '1rem',
-            }}
-          >
-            <img
-              src={auth.user.avatar}
-              alt='auth.user.avatar'
-              style={{
-                borderRadius: '50%',
-                width: '30px',
-                height: '30px',
-                marginRight: '10px',
-              }}
-            />
-            {auth.user.name}
+          <Button className='mr-4 px-2 sm:px-4'>
+            <div className='h-8 w-8'>
+              <img
+                src={auth.user.avatar}
+                alt='auth.user.avatar'
+                className='rounded-full w-full h-full align-middle object-cover'
+              />
+            </div>
+            <div className='ml-3 border-transparent hidden sm:flex'>
+              {auth.user.name}
+              <svg
+                className='-mr-1 ml-2 h-5 w-5'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'
+                fill='currentColor'
+                aria-hidden='true'
+              >
+                <path
+                  fill-rule='evenodd'
+                  d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                  clip-rule='evenodd'
+                />
+              </svg>
+            </div>
           </Button>
         </Link>
-        <Button
-          primary='true'
-          round='true'
-          style={{
-            marginRight: '1rem',
-          }}
-          onClick={handleLogout}
-        >
-          Logout
+        <Button className='mr-4 px-3 md:px-4' onClick={handleLogout}>
+          <div className='hidden md:block md:mr-3'>Logout</div>
+          <GrLogout className=''></GrLogout>
         </Button>
-      </NavBtn>
+      </div>
     )
   }
 
   return (
-    <Nav>
-      <Link href='/' passHref>
-        <NavLink
-          css={`
-            font-size: 1.5rem;
+    <nav className='bg-transparent h-20 flex flex-row z-50 relative justify-between'>
+      <div className='flex flex-row justify-start'>
+        <Link href='/' passHref>
+          <a className='text-black flex items-center no-underline h-full cursor-pointer transition duration-200 ease-in-out text-2xl sm:text-3xl'>
+            Wed2Be
+          </a>
+        </Link>
+        <div className='hidden md:flex flex-row items-center'>
+          {menuData.map((item, index) => (
+            <Link href={item.link} passHref key={index}>
+              <a className='text-gray-500 flex items-center ml-6 lg:ml-14 xl:ml-16 no-underline h-full cursor-pointer transition duration-200 ease-in-out hover:text-black'>
+                {item.title}
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className='flex flex-row justify-end'>
+        {Object.keys(auth).length === 0 ? (
+          <div className='flex items-center'>
+            <Link href='/signin' passHref>
+              {/* <Button className='mr-4'>Sign In</Button> */}
+              <Button className='mr-4 px-3 md:px-4'>
+                <div className='hidden md:block md:mr-3'>Sign In</div>
+                <VscSignIn className=''></VscSignIn>
+              </Button>
+            </Link>
 
-            &:hover {
-              color: #000;
-            }
-          `}
-        >
-          Wed2Be
-        </NavLink>
-      </Link>
-      <Bars onClick={toggle} />
-      <NavMenu>
-        {menuData.map((item, index) => (
-          <Link href={item.link} passHref key={index}>
-            <NavLink>{item.title}</NavLink>
-          </Link>
-        ))}
-      </NavMenu>
+            <Link href='/register' passHref>
+              <Button className='mr-4'>Register</Button>
+            </Link>
+          </div>
+        ) : (
+          loggedRouter()
+        )}
 
-      {Object.keys(auth).length === 0 ? (
-        <NavBtn>
-          <Link href='/signin' passHref>
-            <Button primary='true' round='true'>
-              Sign In
-            </Button>
-          </Link>
-          <Link href='/register' passHref>
-            <Button
-              primary='true'
-              round='true'
-              style={{
-                marginLeft: '1rem',
-              }}
+        {/*FaBars: transform: translate(-100%, 75%); */}
+        <FaBars
+          className='self-center justify-end text-2xl cursor-pointer text-black md:hidden'
+          onClick={toggle}
+        ></FaBars>
+      </div>
+
+      <div className='relative inline-block text-left'>
+        <div>
+          <button
+            type='button'
+            className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'
+            id='options-menu'
+            aria-haspopup='true'
+            aria-expanded='true'
+          >
+            Options
+            <svg
+              className='-mr-1 ml-2 h-5 w-5'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 20 20'
+              fill='currentColor'
+              aria-hidden='true'
             >
-              Register
-            </Button>
-          </Link>
-        </NavBtn>
-      ) : (
-        loggedRouter()
-      )}
-    </Nav>
+              <path
+                fill-rule='evenodd'
+                d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                clip-rule='evenodd'
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
+          <div
+            className='py-1'
+            role='menu'
+            aria-orientation='vertical'
+            aria-labelledby='options-menu'
+          >
+            <a
+              href='#'
+              className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+              role='menuitem'
+            >
+              Account settings
+            </a>
+            <a
+              href='#'
+              className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+              role='menuitem'
+            >
+              Support
+            </a>
+            <a
+              href='#'
+              className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+              role='menuitem'
+            >
+              License
+            </a>
+            <form method='POST' action='#'>
+              <button
+                type='submit'
+                className='block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900'
+                role='menuitem'
+              >
+                Sign out
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
 export default Navbar
 
-const Nav = styled.nav`
+/* const Nav = styled.nav`
   background: transparent;
   height: 80px;
   display: flex;
@@ -167,4 +233,4 @@ const NavBtn = styled.div`
   @media screen and (max-width: 768px) {
     display: none;
   }
-`
+` */
