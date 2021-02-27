df6ff51163cbfb119adbf73229be38f4bf67ea15
Andreas Alfvén <andreas.larsson95@gmail.com>
2021-02-27 18:13:32 +0100

    Fixed dropdown in navbar for when logged on as admin. Next up is to continue on admin.

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,34 +25,74 @@ const Navbar = ({ toggle }) => {
   const loggedRouter = () => {
     return (
       <div className='items-center flex'>
-        <Link href='/profile' passHref>
-          <Button className='mr-4 px-2 sm:px-4'>
-            <div className='h-8 w-8'>
-              <img
-                src={auth.user.avatar}
-                alt='auth.user.avatar'
-                className='rounded-full w-full h-full align-middle object-cover'
-              />
-            </div>
-            <div className='ml-3 border-transparent hidden sm:flex'>
-              {auth.user.name}
-              <svg
-                className='-mr-1 ml-2 h-5 w-5'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                aria-hidden='true'
+        <div className='dropdown_btn relative inline-block text-left mr-4'>
+          <div className=''>
+            <Link href='/profile' passHref>
+              <Button className='px-2 sm:px-4'>
+                <div className='h-8 w-8'>
+                  <img
+                    src={auth.user.avatar}
+                    alt='auth.user.avatar'
+                    className='rounded-full w-full h-full align-middle object-cover'
+                  />
+                </div>
+                <div className='ml-3 border-transparent hidden sm:flex'>
+                  {auth.user.name}
+                  {auth.user.role === 'admin' && (
+                    <svg
+                      className='-mr-1 ml-2 h-5 w-5'
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 20 20'
+                      fill='currentColor'
+                      aria-hidden='true'
+                    >
+                      <path
+                        fill-rule='evenodd'
+                        d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
+                        clip-rule='evenodd'
+                      />
+                    </svg>
+                  )}
+                </div>
+              </Button>
+            </Link>
+          </div>
+
+          {auth.user.role === 'admin' && (
+            <div className='origin-top-right absolute right-0 w-full shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden sm:block'>
+              <div
+                className='py-1 dropdown'
+                role='menu'
+                aria-orientation='vertical'
+                aria-labelledby='options-menu'
               >
-                <path
-                  fill-rule='evenodd'
-                  d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-                  clip-rule='evenodd'
-                />
-              </svg>
+                <a
+                  href='#'
+                  className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                  role='menuitem'
+                >
+                  Users
+                </a>
+                <a
+                  href='#'
+                  className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                  role='menuitem'
+                >
+                  Products
+                </a>
+                <a
+                  href='#'
+                  className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                  role='menuitem'
+                >
+                  Categories
+                </a>
+              </div>
             </div>
-          </Button>
-        </Link>
-        <Button className='mr-4 px-3 md:px-4' onClick={handleLogout}>
+          )}
+        </div>
+
+        <Button className='px-3 md:px-4' onClick={handleLogout}>
           <div className='hidden md:block md:mr-3'>Logout</div>
           <GrLogout className=''></GrLogout>
         </Button>
@@ -99,76 +139,9 @@ const Navbar = ({ toggle }) => {
 
         {/*FaBars: transform: translate(-100%, 75%); */}
         <FaBars
-          className='self-center justify-end text-2xl cursor-pointer text-black md:hidden'
+          className='ml-4 self-center justify-end text-2xl cursor-pointer text-black md:hidden'
           onClick={toggle}
         ></FaBars>
-      </div>
-
-      <div className='relative inline-block text-left'>
-        <div>
-          <button
-            type='button'
-            className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500'
-            id='options-menu'
-            aria-haspopup='true'
-            aria-expanded='true'
-          >
-            Options
-            <svg
-              className='-mr-1 ml-2 h-5 w-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-              aria-hidden='true'
-            >
-              <path
-                fill-rule='evenodd'
-                d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-                clip-rule='evenodd'
-              />
-            </svg>
-          </button>
-        </div>
-
-        <div className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
-          <div
-            className='py-1'
-            role='menu'
-            aria-orientation='vertical'
-            aria-labelledby='options-menu'
-          >
-            <a
-              href='#'
-              className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
-              role='menuitem'
-            >
-              Account settings
-            </a>
-            <a
-              href='#'
-              className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
-              role='menuitem'
-            >
-              Support
-            </a>
-            <a
-              href='#'
-              className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
-              role='menuitem'
-            >
-              License
-            </a>
-            <form method='POST' action='#'>
-              <button
-                type='submit'
-                className='block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900'
-                role='menuitem'
-              >
-                Sign out
-              </button>
-            </form>
-          </div>
-        </div>
       </div>
     </nav>
   )
