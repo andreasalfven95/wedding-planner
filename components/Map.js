502f0378c6a0cc9f363b110b71f881dc5b43db3f
unknown <andreas.alfven95@gmail.com>
2021-07-13 16:06:29 +0200

    Popup 'mark as favourite' now works. Next is to make the map NOT zoom out after clicking the heart-icon

--- components/Map.js
+++ components/Map.js
@@ -7,16 +7,79 @@ import 'leaflet-defaulticon-compatibility'
 import { useEffect, useState } from 'react'
 import MarkerClusterGroup from 'react-leaflet-markercluster'
 import 'react-leaflet-markercluster/dist/styles.min.css'
+import { Button } from './Button'
+import { addToCart, deleteItem } from '../store/Actions'
+import Link from 'next/link'
+import { useContext } from 'react'
+import { DataContext } from '../store/GlobalState'
+
+import {AiOutlineMail, AiOutlinePhone} from "react-icons/ai"
+import {HiHeart, HiOutlineHeart} from "react-icons/hi"
 
 const Map = ({ productsToDisplay }) => {
   /* const [activeMarker, setActiveMarker] = useState(null) */
   const [center, setCenter] = useState([62.597203, 16.491682])
   const [zoom, setZoom] = useState(4)
 
+  const { state, dispatch } = useContext(DataContext)
+  const { cart, auth } = state
+
   const icon = new Icon({
     iconUrl: '/icons/marker_icon.svg',
     iconSize: [35, 35],
   })
+
+  let isAdded = false
+
+  /* const printer = ({item}) => {
+    if (item !== undefined) {
+      return(
+        <>
+          <h1>{item._id}</h1>
+        </>
+      )
+    }
+    else{
+      return(
+        <h1>hej där, inga items</h1>
+      )
+    }
+  } */
+
+  
+
+  const checkCart = ({item}) => {
+    const check = cart.every((product) => {
+      return product._id !== item._id
+    })
+
+    if (!check) {
+      isAdded = true
+    }
+
+    if (isAdded === false) {
+      return (
+        <Button
+          className='w-min p-1'
+          primary='true'
+          onClick={() => dispatch(addToCart(item, cart))}
+        >
+          <HiOutlineHeart className='text-3xl p-0 min' />
+        </Button>
+      )
+    }
+    if (isAdded === true) {
+      return (
+        <Button
+          className='w-min p-1'
+          primary='true'
+          onClick={() => dispatch(deleteItem(cart, item._id, 'ADD_CART'))}
+        >
+          <HiHeart className='text-3xl p-0 min' />
+        </Button>
+      )
+    }
+  }
 
   /* function Bounds({ coordinates }) {
     const map = useMap()
@@ -29,7 +92,7 @@ const Map = ({ productsToDisplay }) => {
   } */
 
   return (
-    <div id='mapid' className='w-screen h-96 z-0 mt-4'>
+    <div id='mapid' className='w-full h-96 z-0 mt-4'>
       <MapContainer
         /* whenCreated={map => OpenStreetMapProvider(map)} */
         /* bounds={coordinates} */
@@ -38,12 +101,14 @@ const Map = ({ productsToDisplay }) => {
         center={center}
         zoom={zoom}
         scrollWheelZoom={false}
+        tap={false}
         style={{ height: '100%', width: '100%', zIndex: '0' }}
       >
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          /* , Imagery © <a href="https://www.mapbox.com/">Mapbox</a> */
+          /* url='https://api.mapbox.com/styles/v1/andreasalfven95/ckqy4vrv903uk18pat6xg6g6p/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmVhc2FsZnZlbjk1IiwiYSI6ImNrcjF1d3poYTBiYWIydnB0dmR5cG8yMTEifQ.RxPDeo0ikm1qPahM4C-UaA'
+          attribution='&copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>' */
         />
 
         <MarkerClusterGroup showCoverageOnHover={false}>
@@ -60,10 +125,50 @@ const Map = ({ productsToDisplay }) => {
               }} */
               icon={icon}
             >
-              <Popup /* minWidth={200} */>
-                <div>
-                  <b>Hello world!</b>
-                  <p>I am a clustered popup.</p>
+              <Popup maxWidth={200}>
+                <div className="flex flex-col">
+                  <h2 className="capitalize text-lg font-bold">{item.title}</h2>
+                  <div className="">
+                    <img className="block max-h-32 md:max-w-sm mx-auto md:m-0" layout='fill' src={item.images[0].url} alt={item.images[0].url}></img>
+                  </div>
+                  <p className="my-0">{item.description}</p>
+
+                  {item.email && (
+                      <div className='flex items-center mb-1'>
+                      <div className="w-min self-start text-beige-normal inline-block mr-4 mt-1">
+                        <AiOutlineMail></AiOutlineMail>
+                      </div>
+                        <a
+                          className='hover:underline'
+                          href={`mailto:${item.email}`}
+                        >
+                          {item.email}
+                        </a>
+                      </div>
+                  )}
+
+                  {item.phone && (
+                      <div className='flex items-center mb-1'>
+                      <div className="w-min self-start text-beige-normal inline-block mr-4 mt-1">
+                        <AiOutlinePhone></AiOutlinePhone>
+                      </div>
+                        <a className='hover:underline' href={`tel:${item.phone}`}>
+                          {item.phone}
+                        </a>
+                      </div>
+                  )}
+
+                  <div className='flex justify-end'>
+                    <Link href={`/product/${item._id}`} passHref>
+                      <Button primary='true' className='mr-2'>
+                        Läs mer...
+                      </Button>
+                      </Link>
+
+                      {checkCart({item})}
+                      {/* <CheckCart>Test</CheckCart> */}
+                  </div>
+                  
                 </div>
               </Popup>
             </Marker>
