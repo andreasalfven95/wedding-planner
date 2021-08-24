0c0409f0c896624a19001f78e651fb73145fc441
Andreas Alfvén <andreas.alfven95@gmail.com>
2021-08-24 13:27:43 +0200

    Fixing the API routes.

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -5,12 +5,11 @@ export const imageUpload = async (images) => {
     formData.append('file', item)
     formData.append(
       'upload_preset',
-      process.env.CLOUD_UPDATE_PRESET
-      /* process.env.NEXT_PUBLIC_CLOUD_UPDATE_PRESET */
+      process.env.NEXT_PUBLIC_CLOUD_UPDATE_PRESET
     )
-    formData.append('upload_preset', process.env.CLOUD_UPDATE_PRESET)
-    formData.append('cloud_name', process.env.CLOUD_NAME)
-    /* formData.append('cloud_name', process.env.NEXT_PUBLIC_CLOUD_NAME) */
+    /* formData.append('upload_preset', process.env.CLOUD_UPDATE_PRESET) */
+    /* formData.append('cloud_name', process.env.CLOUD_NAME) */
+    formData.append('cloud_name', process.env.NEXT_PUBLIC_CLOUD_NAME)
 
     /* const res = await fetch(process.env.CLOUD_API, { */
     const res = await fetch(process.env.NEXT_PUBLIC_CLOUD_API, {
